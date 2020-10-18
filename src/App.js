654aef09e0190799b8f4a4bb9950fd1675f08437
Thrifty Kapila <thriftygsf@gmail.com>
2020-10-18 14:40:17 +0530

    :rocket: filtering todos based on status

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 import "./App.css";
 import TodoList from "./components/TodoList";
 function App() {
+  // state
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //Use Effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  };
+
   return (
     <div className="App">
       <header>
@@ -15,8 +38,13 @@ function App() {
         todos={todos}
         setTodos={setTodos}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
