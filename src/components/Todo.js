e7bd8f43d5fc48f9db405318fe4ba8ff314ea906
SeanLuo-FSWD <shawnluo415@gmail.com>
2020-10-23 15:42:47 -0700

    done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Todo = ({ todoText, todos, setTodos, todo }) => {
+  const textRef = React.useRef();
+  const [btnDisplay, setbtnDisplay] = useState("none");
+  const [btnDropdown, setbtnDropdown] = useState(false);
+
+  useEffect(() => {
+    if (textRef.current.scrollWidth > textRef.current.offsetWidth) {
+      console.log(
+        textRef.current.textContent +
+          " in useEffect " +
+          textRef.current.offsetWidth +
+          " pref " +
+          textRef.current.scrollWidth
+      );
+      setbtnDisplay("block");
+    }
+  });
+
   const deleteHandler = () => {
     setTodos(todos.filter((ele) => ele.id !== todo.id));
+  };
+
+  const toggleDropdown = () => {
+    console.log(btnDropdown);
+    let currentState = btnDropdown;
+    setbtnDropdown(!currentState);
   };
 
   const completeHandler = () => {
@@ -18,15 +41,29 @@ const Todo = ({ todoText, todos, setTodos, todo }) => {
 
   return (
     <div className="todo">
-      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+      <li
+        className={`todo-item ${todo.completed ? "completed" : ""} ${
+          btnDropdown ? "btn_dropdown" : ""
+        }`}
+        ref={textRef}
+      >
         {todoText}
       </li>
-      <button onClick={completeHandler} className="complete-btn">
-        <i className="fas fa-check"></i>
-      </button>
-      <button onClick={deleteHandler} className="trash-btn">
-        <i className="fas fa-trash"></i>
-      </button>
+      <div className="btn_wrap">
+        <button
+          onClick={toggleDropdown}
+          className="complete-btn"
+          style={{ display: `${btnDisplay}` }}
+        >
+          <i className="fas fa-angle-down"></i>
+        </button>
+        <button onClick={completeHandler} className="complete-btn">
+          <i className="fas fa-check"></i>
+        </button>
+        <button onClick={deleteHandler} className="trash-btn">
+          <i className="fas fa-trash"></i>
+        </button>
+      </div>
     </div>
   );
 };
